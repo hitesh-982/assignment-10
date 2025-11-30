e5e174c276490e1a1db69db14cb3e68ab7562e67
Hitesh <coolhitesh298@gmail.com>
2025-11-30 19:19:22 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <queue>;
+#include<unordered_set>
+using namespace std;
+int main(){
+    vector<int> A={1,2,3,4};
+    vector<int> B={3,4,5,6};
+    unordered_set<int> s(A.begin(),A.end());
+    for(int x:B) if(s.count(x)) cout<<x<<" ";
+}
