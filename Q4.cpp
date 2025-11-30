2fa1711e8c74799f451aadd43f8fdbf35e865e5c
Hitesh <coolhitesh298@gmail.com>
2025-11-30 19:20:59 +0530

    Create Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>;
+#include<unordered_set>
+#include<unordered_map>
+
+using namespace std;
+
+int main(){
+    vector<int> a={4,5,1,2,0,4};
+    unordered_map<int,int> mp;
+    for(int x:a) mp[x]++;
+    for(int x:a){
+        if(mp[x]==1){
+            cout<<x;
+            break;
+        }
+    }
+}
