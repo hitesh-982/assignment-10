741f020f7864834dc443522b28c35e06e0d58ea7
Hitesh <coolhitesh298@gmail.com>
2025-11-30 19:17:31 +0530

    Create Q-6.cpp

---
+++ Q-6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include<unordered_set>
+using namespace std;
+
+struct Node{
+    int val;
+    Node* left;
+    Node* right;
+    Node(int v):val(v),left(NULL),right(NULL){}
+};
+
+bool dfs(Node* root, unordered_set<int> &s){
+    if(!root) return false;
+    if(s.count(root->val)) return true;
+    s.insert(root->val);
+    return dfs(root->left,s) || dfs(root->right,s);
+}
+
+int main(){
+    Node* root=new Node(5);
+    root->left=new Node(3);
+    root->right=new Node(8);
+    root->left->left=new Node(3);
+    unordered_set<int> s;
+    if(dfs(root,s)) cout<<"Duplicates Found";
+    else cout<<"No Duplicates";
+}
